7c7381f302363ae0cfea989dc13036e5040f031a
Alex Selehenenko <salex220289@gmail.com>
2021-10-06 16:47:03 +0200

    #15 add services interfaces v2

---
+++ src/Promocodes.Business/Services/InterfacesV2/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using Promocodes.Data.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Promocodes.Business.Services.InterfacesV2
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Shop>> GetShopsAsync(int categoryId);
+    }
+}
---
+++ src/Promocodes.Business/Specifications/Categories/CategoryWithShopsSpecification.cs
@@ -0,0 +1,17 @@
+﻿using Promocodes.Data.Core.Common.Specifications;
+using Promocodes.Data.Core.Entities;
+using System;
+using System.Linq.Expressions;
+
+namespace Promocodes.Business.Specifications.Categories
+{
+    public class CategoryWithShopsSpecification : SpecificationBase<Category>
+    {
+        private CategoryWithShopsSpecification(Expression<Func<Category, bool>> criteria) : base(criteria)
+        {
+            AddInclude(c => c.Shops);
+        }
+
+        public static CategoryWithShopsSpecification ById(int id) => new(c => c.Id == id);
+    }
+}
